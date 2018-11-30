026435a3b1521d1289f95bec179c3a158eb3e3e1
domreader <33025225+domreader@users.noreply.github.com>
2018-11-30 12:40:44 +0000

    Adding to the existing project
    Expanding on images
    Adding scene manager

---
+++ Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	private void StartGame () {
+
+        SceneManager.LoadScene("Main Game");
+
+	}
+	
+	private void MenuScreen()
+    {
+
+        SceneManager.LoadScene("Menu Screen");
+
+    }
+
+}
