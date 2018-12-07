74f526c5b7c52f49e6f499b790fae2b19b569da3
domreader <33025225+domreader@users.noreply.github.com>
2018-12-07 12:30:50 +0000

    Adding few more items into file

---
+++ Project/Assets/Scripts/SpinBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinBall : MonoBehaviour {
+    Transform transform;
+
+    [SerializeField]
+    float smooth = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+    }
+}
