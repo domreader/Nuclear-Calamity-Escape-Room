794071e8dc403aab408b2a3ac59c8d5b8dbfc87c
domreader <33025225+domreader@users.noreply.github.com>
2019-01-13 20:55:34 +0000

    Major Updates - 13/01/19
    Adding functionality, door works and first objective of activating ventialtion is in

---
+++ Project/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenDoor : MonoBehaviour {
+
+    Animation anim;
+    Collider col;
+    bool isEntered;
+
+
+    [SerializeField]
+    private Text text;
+
+    // Use this for initialization
+    void Start () {
+
+        bool isEntered;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.E) && isEntered == true)
+        {
+            this.GetComponent<Animator>().SetTrigger("isOpen");
+
+            text.text = ("Game Complete");
+
+            Debug.Log("In Collider");
+
+        }
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isEntered = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isEntered = false;
+    }
+
+}
---
+++ Project/Assets/Scripts/OpenVents.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenVents : MonoBehaviour {
+
+
+    Animation anim;
+
+    [SerializeField]
+    private Text text;
+
+
+    // Use this for initialization
+    void Start () {
+	
+        
+        
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            this.GetComponent<Animator>().SetTrigger("ventUp");
+
+            text.text = ("Slow Down Reactions");
+
+            Debug.Log("In Collider");
+
+        }
+
+    }
+}
