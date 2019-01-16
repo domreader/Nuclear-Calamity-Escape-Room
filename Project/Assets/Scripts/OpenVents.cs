6aabe9ea3997be2392389459d4c3f3e5db37e4df
domreader <33025225+domreader@users.noreply.github.com>
2019-01-16 12:25:23 +0000

    Changing code so interactibles work

--- Project/Assets/Scripts/OpenVents.cs
+++ Project/Assets/Scripts/OpenVents.cs
@@ -6,33 +6,64 @@ using UnityEngine.UI;
 public class OpenVents : MonoBehaviour {
 
 
-    Animation anim;
+    bool ventConsoleRange;
 
     [SerializeField]
     private Text text;
+    [SerializeField]
+    private Text uiText;
 
 
     // Use this for initialization
     void Start () {
-	
-        
-        
 
-	}
+
+        uiText.gameObject.SetActive(true);
+
+
+    }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (ventConsoleRange == true)
+        {
+
+            uiText.gameObject.SetActive(true);
+
+            uiText.text = ("Press R to open the door");
+        }
+
+        if (ventConsoleRange == false)
+        {
+
+            uiText.gameObject.SetActive(false);
+
+        }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             this.GetComponent<Animator>().SetTrigger("ventUp");
 
-            text.text = ("Slow Down Reactions");
+            text.text = ("Now Slow Down Reactions");
 
             Debug.Log("In Collider");
 
         }
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ventConsoleRange = true;
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ventConsoleRange = false;
+    }
+
 }
+
---
+++ Project/Assets/Scripts/RodLower.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RodLower : MonoBehaviour {
+
+    bool consoleRange;
+
+    [SerializeField]
+    private Text text;
+    [SerializeField]
+    private Text uiText;
+
+    // Use this for initialization
+    void Start () {
+
+        uiText.gameObject.SetActive(true);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+        if (consoleRange == true)
+        {
+
+            uiText.gameObject.SetActive(true);
+
+            uiText.text = ("Press Q to lower rods");
+        }
+
+
+        if (consoleRange == false)
+        {
+
+            uiText.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && consoleRange == true)
+        {
+
+            text.text = ("You have activated the rods, ESCAPE!");
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && consoleRange == false)
+        {
+
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        consoleRange = true;
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        consoleRange = false;
+    }
+}
